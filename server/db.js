765b6b75c038105f9f6dbf6546d4ed96590d302a
Kayla Frankum <frankumk@gmail.com>
2020-12-16 09:24:31 -0800

    searching for heroku issue

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@
 const Sequelize = require('sequelize')
 const { STRING, INTEGER, DATE } = Sequelize
 
-const db = new Sequelize(process.env.DTABASE_URL || 'postgres://localhost/review_lines_db');
+const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/review_lines_db');
 
 const Category = db.define('category',{
     name: {
